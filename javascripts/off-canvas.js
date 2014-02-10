09011c5264e019497144b0b60f2298478210b438
Tou Neko <touneko@gmail.com>
2014-02-10 13:24:07 +0800

    starter init

---
+++ javascripts/off-canvas.js
@@ -0,0 +1,14 @@
+var off_canvas = function (){
+
+  $(".off-canvas-toggle-btn").on("click", function(event){
+    $("body").toggleClass("open-menu");
+    event.preventDefault();
+  });
+
+  $(".outside a, .off-canvas-mask").on("click", function(){
+    $("body").removeClass("open-menu");
+  });
+
+};
+
+off_canvas();
