f5cb0fbec2d443242736591f61c530a4f150e371
Tou Neko <touneko@gmail.com>
2014-02-12 21:59:26 +0800

    add sub navigation

--- javascripts/all.js
+++ javascripts/all.js
@@ -1,2 +1,8 @@
 // 移動 dom
 $( ".main-nav ul" ).appendAround();
+
+// sub-nav trigger
+$(".sub-nav-trigger").on("click",function(){
+  $(".sub-nav,.sub-nav-trigger").toggleClass("open");
+  event.preventDefault();
+});
