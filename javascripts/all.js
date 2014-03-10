e3b731922da968a03fb04de21432a326cc3e2d5a
Tou Neko <touneko@gmail.com>
2014-03-11 01:43:04 +0800

    add sub-nav, collapse-mod, equal height js

--- javascripts/all.js
+++ javascripts/all.js
@@ -6,10 +6,52 @@ $(".sub-nav-title").on("click",function(){
   $(".sub-nav,.sub-nav-title").toggleClass("open");
   event.preventDefault();
 });
+$(".nav li").on("click",function(){
+  $(".sub-nav,.sub-nav-title").delay(50).queue(function(){
+    $(".sub-nav,.sub-nav-title").removeClass("open").dequeue();
+  });
+});
 
-//hc affix
-$('.affix').hcSticky({
+// bs-carousel
+$('.carousel').carousel();
+
+//hc sticky
+$('.aside-sticky').hcSticky({
   top: 20,
   responsive: true,
   offResolutions: -940
+});
+
+$('.nav-sticky').hcSticky({
+  top: -1,
+  noContainer: true,
+  responsive: true,
+});
+
+//collapse button switch content
+$('.collapse-toggle-button').click(function(){
+  $(this).toggleClass('close');
+  if($(this).text() == '展開'){
+    $(this).text('收合');
+  } else {
+    $(this).text('展開');
+  }
+});
+
+// collapse switch accord dom height
+$('.collapse-content').each(function(){
+  if ( $(this).height() < 400 ) {
+    $(this).parent().css("height","auto");
+    $(this).parent().next('.collapse-toggle').remove();
+  }
+});
+
+// equal height
+$('.mktreport').each(function(){
+  var highestBox = 0;
+  $('.item-text', this).each(function(){
+  if($(this).height() > highestBox)
+     highestBox = $(this).height();
+  });
+  $('.item-text',this).height(highestBox);
 });
